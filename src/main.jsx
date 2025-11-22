90b01d2da4de9e37da866a3ad5c2074ff800afd9
PauSintesP <133574903+PauSintesP@users.noreply.github.com>
2025-11-22 19:10:20 +0100

    mejora para poder conectar api

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,8 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+
   <StrictMode>
     <App />
   </StrictMode>,
-)
-
-app.add_middleware(
-  CORSMiddleware,
-  allow_origins = [
-    "http://localhost:5173",
-    "https://web-njoy.vercel.app/",
-    "https://*.vercel.app",
-  ],
-  allow_credentials = True,
-  allow_methods = ["*"],
-  allow_headers = ["*"],
 )
